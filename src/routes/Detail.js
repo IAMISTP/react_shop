d949c95752e5e6c5726e5f1da1a5804e10b77cd6
박혜민 <hyeminpak@gmail.com>
2023-07-08 21:53:24 +0900

    2초가 지나면 요소 숨기기 기능 구현

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { styled } from "styled-components";
 
@@ -10,15 +11,28 @@ let Box = styled.div`
   background-color: grey;
   padding: 20px;
 `;
+
 //기존 스타일 복사도 가능
 let NewBtn = styled.button(YellowBtn);
 export function Detail(props) {
   let { id } = useParams();
   let num = parseInt(id) - 1;
   let data = props.shoes.find((x) => x.id.toString() === num.toString());
+  let [styleDisplay, setStyleDisplay] = useState("visible");
+  useEffect(() => {
+    setTimeout(() => {
+      setStyleDisplay("hidden");
+    }, 2000);
+  });
   return (
     <>
       <div className="container">
+        <div
+          className="alert alert-warning "
+          style={{ visibility: styleDisplay }}
+        >
+          2초 이내 구매시 할인
+        </div>
         <Box>
           <YellowBtn bg="blue">버튼</YellowBtn>
           <YellowBtn bg="orange">버튼</YellowBtn>
